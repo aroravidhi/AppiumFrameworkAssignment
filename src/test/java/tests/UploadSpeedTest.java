182d0325dc7c44fdd27b1c265379c2e419e59760
Vidhi <vidhi@Taruns-MacBook-Air.local>
2025-05-16 16:48:54 +0530

    Initial commit of Appium automation framework

--- src/test/java/tests/UploadSpeedTest.java
+++ src/test/java/tests/UploadSpeedTest.java
@@ -1,4 +1,23 @@
 package tests;
 
-public class UploadSpeedTest {
+import base.BaseTest;
+import org.testng.annotations.*;
+import utils.RetryAnalyzer;
+
+public class UploadSpeedTest extends BaseTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        initializeDriver("Upload Speed Test");
+    }
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void testUploadSpeed() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        quitDriver();
+    }
 }
